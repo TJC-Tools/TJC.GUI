1662aad195f008456386cda3e220343d65030bd4
Tyler Carrol <tylercarrol@hotmail.com>
2024-09-10 19:49:14 -0400

    Create Menu Settings

---
+++ TJC.GUI/Menu/MenuSettings.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using TJC.Singleton;
+
+namespace TJC.GUI.Menu;
+
+internal class MenuSettings : SingletonBase<MenuSettings>
+{
+    internal required Assembly Assembly { get; set; }
+}
