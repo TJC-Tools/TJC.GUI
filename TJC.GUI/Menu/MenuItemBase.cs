449b5f031a79924678fa9bd127848a0b9877af1c
Tyler Carrol <tylercarrol@hotmail.com>
2024-09-10 20:25:01 -0400

    Create MenuItemBase

---
+++ TJC.GUI/Menu/MenuItemBase.cs
@@ -0,0 +1,32 @@
+﻿using TJC.GUI.Menu.Commands;
+
+namespace TJC.GUI.Menu;
+
+internal abstract class MenuItemBase : IMenuItem
+{
+    public abstract string Header { get; }
+
+    public MenuItem GetMenuItem()
+    {
+        var command = new RelayCommand(Execute, CanExecute);
+        var subMenuItems = GetSubMenuItems().Select(x => x.GetMenuItem()).ToList();
+        var menuItem = new MenuItem
+        {
+            Header = Header,
+            Command = command,
+            ItemsSource = subMenuItems
+        };
+        return menuItem;
+    }
+
+    protected virtual IEnumerable<ISubMenuItem> GetSubMenuItems()
+    {
+        return [];
+    }
+
+    protected virtual void Execute(object? obj)
+    {
+    }
+
+    protected virtual bool CanExecute(object? obj) => true;
+}
