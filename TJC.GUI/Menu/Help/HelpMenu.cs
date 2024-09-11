7935b8989f3c1d5cb7ef9082c3e8380302a733c1
Tyler Carrol <tylercarrol@hotmail.com>
2024-09-10 20:42:01 -0400

    Create Help Menu

---
+++ TJC.GUI/Menu/Help/HelpMenu.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TJC.GUI.Menu.Help;
+
+internal class HelpMenu : MenuItemBase, IMainMenuItem
+{
+    public override string Header => "Help";
+
+    protected override IEnumerable<ISubMenuItem> GetSubMenuItems()
+    {
+        yield return new AboutMenuItem();
+    }
+}
