34b27f50c98e2806d39f0ede1763d0d4664ced1b
Tyler Carrol <tylercarrol@hotmail.com>
2024-09-10 20:43:07 -0400

    Create MainView & MainViewModel for Sample Application

--- SampleGUIApplication/MainWindow.xaml.cs
+++ SampleGUIApplication/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using SampleGUIApplication.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SampleGUIApplication
 {
@@ -19,6 +11,8 @@ namespace SampleGUIApplication
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ SampleGUIApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using TJC.GUI.Menu;
+
+namespace SampleGUIApplication.ViewModels
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            MenuItems = MenuFactory.GetMenuItems();
+        }
+
+        public IEnumerable<MenuItem> MenuItems { get; }
+    }
+}
