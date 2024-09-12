c7c1cc386778fecbb18c335c9bba2237d4e6a0f8
Tyler Carrol <tylercarrol@hotmail.com>
2024-09-11 21:13:34 -0400

    Create FileMenu & ExitCommand

---
+++ TJC.GUI/Menu/Items/File/ExitCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace TJC.GUI.Menu.Items.File;
+
+internal class ExitCommand : MenuItemBase, ISubMenuItem
+{
+    public ExitCommand() : base(MenuSettings.Instance.Exit)
+    {
+        if (MenuSettings.Instance.Assembly == null)
+            throw new NullReferenceException("Assembly is null.");
+    }
+
+    public override string Header => "E_xit";
+
+    protected override void Execute(object? obj = null)
+    {
+        Environment.Exit(0);
+    }
+}
