e6bd05ad3ed24cef497e7118d9a476449e3d79bf
Tyler Carrol <tylercarrol@hotmail.com>
2024-09-11 22:36:16 -0400

    Dependencies

--- TJC.GUI/GlobalUsings.cs
+++ TJC.GUI/GlobalUsings.cs
@@ -1,7 +1,7 @@
-﻿global using System.Windows.Controls;
+﻿global using Avalonia.Controls;
+global using ReactiveUI;
 global using TJC.AssemblyExtensions.Attributes;
 global using TJC.GUI.Menu.Base;
-global using TJC.GUI.Menu.Commands;
 global using TJC.GUI.Menu.Extensions;
 global using TJC.GUI.Menu.Interfaces;
 global using TJC.GUI.Menu.Settings;
