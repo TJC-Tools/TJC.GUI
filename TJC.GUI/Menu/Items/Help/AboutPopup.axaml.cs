dc977c3deceb205c7995cd90e329abb553e31c83
Tyler <tylercarrol@hotmail.com>
2024-09-18 20:01:05 -0400

    Improve About Popup Window (#13)

---
+++ TJC.GUI/Menu/Items/Help/AboutPopup.axaml.cs
@@ -0,0 +1,41 @@
+namespace TJC.GUI.Menu.Items.Help
+{
+    public partial class AboutPopup : Window
+    {
+        public AboutPopup()
+        {
+            InitializeComponent();
+        }
+
+        public AboutPopup(string? title = null,
+                          Version? version = null,
+                          string? copyright = null,
+                          string? license = null,
+                          string? thirdPartyLicenses = null) : this()
+        {
+            Title = $"About {title}";
+
+            SetSize();
+
+            VersionTextBlock.Text = $"Version: {version}";
+            VersionTextBlock.IsVisible = version != null;
+
+            CopyrightTextBlock.Text = copyright;
+            CopyrightTextBlock.IsVisible = copyright != null;
+
+            LicenseTextBlock.Text = license;
+            LicenseTextBlock.IsVisible = license != null;
+        }
+
+        private void SetSize()
+        {
+            Width = 500;
+            Height = 500;
+            MinWidth = 150;
+            MinHeight = 150;
+            MaxWidth = 1200;
+            MaxHeight = 800;
+            SizeToContent = SizeToContent.Height;
+        }
+    }
+}
