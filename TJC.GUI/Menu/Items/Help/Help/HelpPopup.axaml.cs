758c548748d755c51485bcb93f53f872c50520f5
Tyler Carrol <tylercarrol@hotmail.com>
2024-09-18 20:54:57 -0400

    Create Help Item & Popup

---
+++ TJC.GUI/Menu/Items/Help/Help/HelpPopup.axaml.cs
@@ -0,0 +1,32 @@
+namespace TJC.GUI.Menu.Items.Help.Help
+{
+    public partial class HelpPopup : Window
+    {
+        public HelpPopup()
+        {
+            InitializeComponent();
+        }
+
+        public HelpPopup(string? title = null,
+                         string? content = null)
+            : this()
+        {
+            Title = $"Help {title}";
+
+            SetSize();
+
+            ContentTextBlock.Text = content;
+        }
+
+        private void SetSize()
+        {
+            Width = 500;
+            Height = 500;
+            MinWidth = 150;
+            MinHeight = 150;
+            MaxWidth = 1200;
+            MaxHeight = 800;
+            SizeToContent = SizeToContent.Height;
+        }
+    }
+}
