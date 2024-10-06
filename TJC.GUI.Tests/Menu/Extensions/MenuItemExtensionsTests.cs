46b6a60f2c5a1090c36e774ef0d3284bdb7b54ed
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-06 10:53:30 -0400

    🚨 tests: Add Tests to Add Sub Menu Item (to start & end)

--- TJC.GUI.Tests/Menu/Extensions/MenuItemExtensionsTests.cs
+++ TJC.GUI.Tests/Menu/Extensions/MenuItemExtensionsTests.cs
@@ -20,4 +20,44 @@ public class MenuItemExtensionsTests
         Assert.IsNotNull(fileMenu);
         Assert.AreEqual("_File", fileMenu.Header);
     }
+
+    [TestMethod]
+    public void AddSubMenuItem_ToStart_IsFirstItem()
+    {
+        // Arrange
+        var menuItems              = MenuFactory.CreateMenuItems().ToList();
+        var fileMenu               = menuItems.FindMenuItem("FILE");
+        var subMenuItemCountBefore = fileMenu?.ItemsSource?.Cast<object>().Count();
+        var newSubMenuItem         = new MenuItem { Header = "New" };
+
+        // Act
+        fileMenu?.AddSubMenuItem(newSubMenuItem, index: 0);
+        var subMenuItemCountAfter = fileMenu?.ItemsSource?.Cast<object>().Count();
+        var firstSubMenuItem      = fileMenu?.ItemsSource?.Cast<object>().First() as MenuItem;
+
+        // Assert
+        Assert.IsNotNull(fileMenu);
+        Assert.AreEqual(subMenuItemCountBefore + 1, subMenuItemCountAfter);
+        Assert.AreEqual(newSubMenuItem.Header, firstSubMenuItem?.Header);
+    }
+
+    [TestMethod]
+    public void AddSubMenuItem_ToEnd_IsLastItem()
+    {
+        // Arrange
+        var menuItems              = MenuFactory.CreateMenuItems().ToList();
+        var fileMenu               = menuItems.FindMenuItem("FILE");
+        var subMenuItemCountBefore = fileMenu?.ItemsSource?.Cast<object>().Count();
+        var newSubMenuItem         = new MenuItem { Header = "New" };
+
+        // Act
+        fileMenu?.AddSubMenuItem(newSubMenuItem);
+        var subMenuItemCountAfter = fileMenu?.ItemsSource?.Cast<object>().Count();
+        var lastSubMenuItem       = fileMenu?.ItemsSource?.Cast<object>().Last() as MenuItem;
+
+        // Assert
+        Assert.IsNotNull(fileMenu);
+        Assert.AreEqual(subMenuItemCountBefore + 1, subMenuItemCountAfter);
+        Assert.AreEqual(newSubMenuItem.Header, lastSubMenuItem?.Header);
+    }
 }
