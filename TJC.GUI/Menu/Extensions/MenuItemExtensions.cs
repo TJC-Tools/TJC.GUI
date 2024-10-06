9775299333add9265304b55411d9cfaefd76bedb
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-06 10:29:23 -0400

    ✨ feature: Add extension to add a sub menu item to a menu

--- TJC.GUI/Menu/Extensions/MenuItemExtensions.cs
+++ TJC.GUI/Menu/Extensions/MenuItemExtensions.cs
@@ -2,7 +2,13 @@
 
 public static class MenuItemExtensions
 {
-    public static MenuItem? GetMenuItem(this IEnumerable<MenuItem> menuItems, string header)
+    /// <summary>
+    /// Locate a menu item by its header.
+    /// </summary>
+    /// <param name="menuItems"></param>
+    /// <param name="header"></param>
+    /// <returns></returns>
+    public static MenuItem? FindMenuItem(this IEnumerable<MenuItem> menuItems, string header)
     {
         // Clean the header and menu item headers to make the search case-insensitive and ignore underscores
         var searchHeaderClean = header.ToLower().Replace("_", string.Empty);
@@ -16,5 +22,32 @@ public static class MenuItemExtensions
         }
 
         return null;
+    }
+
+    /// <summary>
+    /// Add a sub-menu item to a menu item at the specified index.
+    /// If the index is -1 (default), the sub-menu item is added to the end.
+    /// </summary>
+    /// <param name="menuItem"></param>
+    /// <param name="subMenuItem"></param>
+    /// <param name="index"></param>
+    public static void AddSubMenuItem(MenuItem menuItem, MenuItem subMenuItem, int index = -1)
+    {
+        // If the menu item does not have any items, create a new list
+        menuItem.ItemsSource ??= new List<MenuItem>();
+        if (menuItem.ItemsSource is not IEnumerable<object> items)
+            return; // This should never happen
+
+        // Convert the items to a list, so we can add the sub-menu item
+        var menuItems = items.ToList();
+
+        // If the index is -1, add the sub-menu item to the end, otherwise insert at the index
+        if (index == -1)
+            menuItems.Add(subMenuItem);
+        else
+            menuItems.Insert(index, subMenuItem);
+
+        // Update the items source
+        menuItem.ItemsSource = menuItems;
     }
 }
