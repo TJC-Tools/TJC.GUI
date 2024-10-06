9707ebfb67b23ca6b3fe31f1dcb9fbe20700c829
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-06 10:17:01 -0400

    ✨ feature: Add extension to find menu item by its header

---
+++ TJC.GUI/Menu/Extensions/MenuItemExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace TJC.GUI.Menu.Extensions;
+
+public static class MenuItemExtensions
+{
+    public static MenuItem? GetMenuItem(this IEnumerable<MenuItem> menuItems, string header)
+    {
+        // Clean the header and menu item headers to make the search case-insensitive and ignore underscores
+        var searchHeaderClean = header.ToLower().Replace("_", string.Empty);
+        foreach (var menuItem in menuItems)
+        {
+            if (menuItem.Header is not string menuItemHeader)
+                continue;
+            var menuItemHeaderClean = menuItemHeader.ToLower().Replace("_", string.Empty);
+            if (menuItemHeaderClean == searchHeaderClean)
+                return menuItem;
+        }
+
+        return null;
+    }
+}
