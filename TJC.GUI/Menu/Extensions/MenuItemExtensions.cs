d82390e04025a114746c65236b35a062fb2a7849
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-06 10:15:08 -0400

    🧹 chore: Rename extension class & make public

--- TJC.GUI/Menu/Extensions/MenuItemExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TJC.GUI.Menu.Extensions;
-
-internal static class MenuItemExtensions
-{
-    public static IEnumerable<MenuItem> GetMenuItems(this IEnumerable<IMenuItem> menuItems)
-    {
-        return menuItems.Select(x => x.CreateMenuItem()).OfType<MenuItem>();
-    }
-}
