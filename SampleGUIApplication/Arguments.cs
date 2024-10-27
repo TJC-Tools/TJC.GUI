3f298342976ae36485dbb278aa5fa47545c32bfb
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-26 23:00:42 -0400

    ✨ feature: Sample Program - Add Theme Argument

---
+++ SampleGUIApplication/Arguments.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Styling;
+using System.Reflection;
+using TJC.ConsoleApplication.Arguments.Options;
+
+namespace SampleGUIApplication;
+
+internal class Arguments
+{
+    internal static void Parse(string[] args) =>
+        ConsoleArguments.ParseAndValidate(args, Assembly.GetCallingAssembly().GetName().Name, exitOnFailureToParse: false);
+
+    internal static ThemeVariant Theme { get; private set; } = ThemeVariant.Default;
+
+    internal static readonly ConsoleArguments ConsoleArguments = new(flagRequired: true, logParsedOptions: false)
+    {
+        { "theme=", v => Theme = ParseTheme(v), "Theme" }
+    };
+
+    private static ThemeVariant ParseTheme(string theme) =>
+        theme.ToLower() switch
+        {
+            "dark" => ThemeVariant.Dark,
+            "light" => ThemeVariant.Light,
+            "default" => ThemeVariant.Default,
+            _ => throw new NotSupportedException($"[{nameof(Arguments)}] Theme [{theme}] is not supported.")
+        };
+}
