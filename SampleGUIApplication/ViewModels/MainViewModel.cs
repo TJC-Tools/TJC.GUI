b5f3bd87e3bf74e75b6b5c17a3b943195cf9a9fe
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-28 21:53:39 -0400

    Update MainViewModel.cs

--- SampleGUIApplication/ViewModels/MainViewModel.cs
+++ SampleGUIApplication/ViewModels/MainViewModel.cs
@@ -16,12 +16,7 @@ public class MainViewModel : ViewModelBase
             + "This should be a new paragraph.\n"
             + "With multiple lines.\n"
             + "And **Bolding**, *Italics*, & `code format` \n\n"
-            + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ex metus, porttitor in nulla blandit, "
-            + "commodo convallis libero. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
-            + "Vivamus eleifend sollicitudin leo a maximus. Donec a blandit quam, ut aliquet turpis. Quisque tristique, "
-            + "mauris efficitur tempor venenatis, nisi augue lobortis elit, quis laoreet risus nisl vitae massa. "
-            + "Donec a placerat risus. Sed a auctor neque. Curabitur tempor id lacus vel ultrices. Proin blandit non orci non finibus. "
-            + "Nullam posuere nisl enim, vel placerat magna laoreet iaculis.";
+            + "Here is some more example text.";
 
         MenuItems = MenuFactory.CreateMenuItems().ToList();
     }
